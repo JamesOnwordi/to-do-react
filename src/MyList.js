db6571c6a13aab6168bddf1b69d1bab4c161cd1f
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-09-22 21:10:40 -0500

    MyList class made

---
+++ src/MyList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class MyList extends Component{
+  render(){
+    return (
+      <div>
+        <p> This is List's classs</p>
+        <ul>
+          <li>List being made </li>
+        </ul>
+      </div>
+    )
+  }
+}
